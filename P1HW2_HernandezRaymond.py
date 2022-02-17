70f0740d89f6ee11c2acd1a5463fdd3dd158046f
hernandr4055 <99464578+hernandr4055@users.noreply.github.com>
2022-02-17 13:51:14 -0500

    Add files via upload

---
+++ P1HW2_HernandezRaymond.py
@@ -0,0 +1,20 @@
+    # This project will show my ability to use IDLE to calculate pizzas and slices.
+    # 2/17/2022
+    # CTI-110 P1HW2 - Pizza Order
+    # Raymond Hernandez
+    #
+
+students = int(input("How many students do you want to order pizza for?: "))
+slices = students * 3
+pizzas = students / 2
+
+print('----Pizza Order--------')
+print('Number of Students :', students)
+print('Pizza Slices Needed :', slices)
+print('Pizzas Needed :', pizzas)
+print('-----------------------------')
+
+    # Ask for the amount of students getting pizza.
+    # Multiply the amount of students by 3 to get slices.
+    # Divide the amount of students by 2 to get total pizzas.
+    # Print the whole order.
